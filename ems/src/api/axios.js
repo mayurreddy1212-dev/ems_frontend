b196d42dd3fc8e94b99d2eb1343f260fd9e13f65
Mayur <mayurreddy1212@gmail.com>
2026-02-15 14:55:19 +0530

    deployment on vercel

--- ems/src/api/axios.js
+++ ems/src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  // baseURL: "https://mail-agent-2fbo.onrender.com", //deployment
-  baseURL:"http://127.0.0.1:8000", //development
+  baseURL: "https://mail-agent-2fbo.onrender.com", //deployment
+  // baseURL:"http://127.0.0.1:8000", //development
   timeout:10000,
 });
 
